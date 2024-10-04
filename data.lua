947c85b417c2dbd17a860c059b70876f4d83d736
notnotmelon <zacharypicco@gmail.com>
2024-10-04 16:39:26 -0500

    feat: Inital release

---
+++ data.lua
@@ -0,0 +1,9 @@
+local lab = data.raw.lab['biter-lab']
+
+local new_surface_conditions = {}
+for _, condition in pairs(lab.surface_conditions) do
+    if condition.property ~= 'pressure' then
+        new_surface_conditions[#new_surface_conditions + 1] = condition
+    end
+end
+lab.surface_conditions = new_surface_conditions
