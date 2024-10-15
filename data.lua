6033e769c245f22e0c6dd93cda9bec2de276264c
notnotmelon <zacharypicco@gmail.com>
2024-10-14 22:09:57 -0500

    feat: Add error checking in cases of invalid installation

--- data.lua
+++ data.lua
@@ -1,5 +1,9 @@
 local lab = data.raw.lab['biolab']
 
+if not lab then
+    error('Mod "Biolabs in Space" failed to load. ERROR: Could not find biolab prototype. Was this lab removed by another mod?')
+end
+
 local new_surface_conditions = {}
 for _, condition in pairs(lab.surface_conditions) do
     if condition.property ~= 'pressure' then
